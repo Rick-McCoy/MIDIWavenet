Bugfix for gradient accumulation.

model.py
@@ -27,9 +27,9 @@ class Wavenet:
         self.out_channels = args.out_channels
         self.learning_rate = args.learning_rate
         self.optimizer = self._optimizer()
+        self.optimizer.zero_grad()
         self.writer = writer
         self.total = 0
-        self.acc_loss = 0
         self.accumulate = args.accumulate
 
     def _optimizer(self):
@@ -44,15 +44,13 @@ class Wavenet:
         output, loss = self.net(x[:, :, :-1], condition, target)
         loss = loss.sum()
         loss_item = loss.item()
-        self.acc_loss += loss
         if train:
+            loss.backward()
             if step % self.accumulate == self.accumulate - 1:
-                self.optimizer.zero_grad()
-                self.acc_loss.backward()
                 self.optimizer.step()
-                self.acc_loss = 0
+                self.optimizer.zero_grad()
                 self.writer.add_scalar('Train/Loss', loss_item, step // self.accumulate)
-            if step % 20 == 19:
+            if step // self.accumulate % 20 == 19:
                 self.writer.add_image('Score/Real', x[0, :, -output.shape[2]:].unsqueeze(dim=0), step // self.accumulate)
                 self.writer.add_image('Score/Generated', torch.nn.functional.softmax(output[0].unsqueeze(dim=0), dim=1), step // self.accumulate)
         del loss, output